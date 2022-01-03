00829b06b3e2835659ca4d32236322d67e6e3e90
caopeng <caopeng@youzan.com>
2022-01-03 22:39:10 +0800

    feat:  clean up 清理代码

--- company/src/main/java/lambda/package2/test1.java
+++ company/src/main/java/lambda/package2/test1.java
@@ -14,6 +14,12 @@ public class test1 {
 	public static void main(String[] args) {
 		//test flatmap
 		Stream.of(asList(1,2)).flatMap(numer->numer.stream()).collect(Collectors.toList());
+		Runnable r = new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("Hello World!");
+			}
+		};
 	}
 
 	private static ArrayList<Integer> asList(Integer ... a) {
---
+++ company/src/main/java/lambda/设计模式/创建形模式/single.java
@@ -0,0 +1,10 @@
+package lambda.设计模式.创建形模式;
+
+/**
+ * @description: 单例模式
+ * @author:maidang
+ * @date:2022/01/02
+ **/
+public class single {
+
+}
